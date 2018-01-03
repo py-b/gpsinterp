c6f03e0d1f9e7875b24f759ca4f7096f99a9c590
Pierre-Yves Berrard <pierre.yves.berrard@gmail.com>
2018-01-03 01:41:36 +0100

    improve message after listing images

--- R/exported.R
+++ R/exported.R
@@ -1,5 +1,7 @@
 #' @export
 list_images <- function(path = ".") {
+
+  # list images in "input"
   res <-
     tibble::tibble(
       PHOTO = list.files(
@@ -7,23 +9,35 @@ list_images <- function(path = ".") {
         pattern = "\\.JPG$" #jpg, jpeg
       )
     )
+
   message(
     paste0(
-      nrow(res), " images found in\n ",
-      normalizePath(path, winslash = "/"), "/input/photos\n",
-      "First : ", res$PHOTO[1], "\n",
-      " Last : ", res$PHOTO[nrow(res)], "\n"
+      nrow(res), " images found in \"",
+      normalizePath(path, winslash = "/"), "/input/photos\"\n",
+      " - First : ", res$PHOTO[1], "\n",
+      " - Last  : ", res$PHOTO[nrow(res)], "\n",
+      "Now, open JOSM. In a new layer, create at least two nodes :\n",
+      "the first and last image filename (in \"name\" attribute).\n",
+      "Save as \"input/approx.osm\"."
     )
   )
+
+  # stores images list in temporary environment
+  # tmp_env <- new.env()
+  # assign("images", res, envir = tmp_env)
+
   res
+
 }
 
 #' @export
 read_exact <- function(path = ".") {
+
   read_positions(
     file.path(path, "input", "approx.osm")
   ) %>%
   dplyr::filter(EXACT)
+
 }
 
 #' @export
